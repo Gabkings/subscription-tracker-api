06277cb545487ad2fd5ed13c5a0d03b261ba5108
gabkings <gabworks51@gmail.com>
2026-01-10 18:07:37 +0300

    Adding rate limiting using arcjet library

--- config/env.js
+++ config/env.js
@@ -7,7 +7,8 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const {
   PORT, NODE_ENV, MONGO_URI,
-  JWT_SECRET, JWT_EXPIRES_IN
+  JWT_SECRET, JWT_EXPIRES_IN,
+   ARCJET_ENV, ARCJET_KEY
  
 } = process.env;
 
